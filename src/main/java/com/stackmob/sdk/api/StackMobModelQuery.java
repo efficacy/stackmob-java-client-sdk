db9a7ee85b46fa74df12cd5750d7761140798cbe
Douglas Rapp <drapp@stackmob.com>
2012-03-05 15:05:32 -0800

    Add StackMobModelQuery A query that returns actual objects. This implementation uses generics and is almost awesome. The only downside is that you have to remember to do some magic when constructing them new StackMobModelQuery<MyModel>(){} This means that you're actually constructing an anonymous subclass of StackMobModelQuery<MyModel> on the fly, thus causing java to hang onto the type information we need to pass on to gson so it can spit out real classes. If that's too much for people this can be changed to take MyModel.class as an input parameter instead.

---
+++ src/main/java/com/stackmob/sdk/api/StackMobModelQuery.java
@@ -0,0 +1,82 @@
+/**
+ * Copyright 2012 StackMob
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.stackmob.sdk.api;
+
+import com.google.gson.*;
+import com.stackmob.sdk.callback.StackMobCallback;
+import com.stackmob.sdk.callback.StackMobQueryCallback;
+import com.stackmob.sdk.exception.StackMobException;
+
+import java.lang.reflect.ParameterizedType;
+import java.lang.reflect.Type;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+public class StackMobModelQuery<T extends StackMobModel>{
+
+    Type genericType;
+    StackMobQuery query;
+
+    public StackMobModelQuery() {
+        Type t = getClass().getGenericSuperclass();
+        ParameterizedType pt = (ParameterizedType) t;
+        genericType = pt.getActualTypeArguments()[0];
+        query = new StackMobQuery(((Class)genericType).getSimpleName().toLowerCase());
+    }
+    
+    public StackMobQuery getQuery() {
+        return query;
+    }
+
+    public void send(StackMobQueryCallback<T> callback) {
+        final StackMobQueryCallback<T> furtherCallback = callback;
+        StackMob.getStackMob().get(query, new StackMobCallback() {
+            @Override
+            public void success(String responseBody) {
+                JsonArray array = new JsonParser().parse(responseBody).getAsJsonArray();
+                List<T> resultList = new ArrayList<T>();
+                Iterator<JsonElement> it = array.iterator();
+                while(it.hasNext()) {
+                    resultList.add((T) new Gson().fromJson(it.next(), genericType));
+                }
+                furtherCallback.success(resultList);
+            }
+
+            @Override
+            public void failure(StackMobException e) {
+                furtherCallback.failure(e);
+            }
+        });
+    }
+
+    /*
+    public void send(StackMobQueryCallback<T> callback) {
+        final StackMobQueryCallback<T> furtherCallback = callback;
+        StackMob.getStackMob().get(this, new StackMobCallback() {
+            @Override
+            public void success(String responseBody) {
+                furtherCallback.success((List<T>)new Gson().fromJson(responseBody,responseType));
+            }
+
+            @Override
+            public void failure(StackMobException e) {
+                furtherCallback.failure(e);
+            }
+        });
+    }
+    */
+}
